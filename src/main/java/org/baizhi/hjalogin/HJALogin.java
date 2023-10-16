531f07306cc93e9589a2ab2be14ab740a732de77
baizhibubai <147310119+baizhibubai@users.noreply.github.com>
2023-10-16 19:10:33 +0800

    Update HJALogin.java

--- src/main/java/org/baizhi/hjalogin/HJALogin.java
+++ src/main/java/org/baizhi/hjalogin/HJALogin.java
@@ -1,14 +1,11 @@
 package org.baizhi.hjalogin;
 
-import jdk.jpackage.internal.Log;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public final class HJALogin extends JavaPlugin implements Runnable {
+public final class HJALogin extends JavaPlugin {
     static JavaPlugin instance;
     @Override
     public void onLoad(){
@@ -21,7 +18,7 @@ public final class HJALogin extends JavaPlugin implements Runnable {
         Bukkit.getPluginManager().registerEvents(new EventListener(),this);
         Objects.requireNonNull(Bukkit.getPluginCommand("login")).setExecutor(new Command());
         Objects.requireNonNull(Bukkit.getPluginCommand("register")).setExecutor(new Command());
-        new Thread(new HJALogin()).start();
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this,new InfoRegOrLog(),0,60);
         instance = this;
     }
 
@@ -29,22 +26,5 @@ public final class HJALogin extends JavaPlugin implements Runnable {
     public void onDisable() {
         // Plugin shutdown logic
         saveConfig();
-    }
-
-    @Override
-    public void run() {
-        while (!LoginData.isNull()){
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ignored) {
-            }
-            for (Player player:Bukkit.getOnlinePlayers()){
-                if (LoginData.hasList(player.getName())&&!ConfigReader.isRegister(player.getName())){
-                    player.sendMessage(ChatColor.YELLOW+"请输入/register <密码> <确认密码>");
-                }else if (LoginData.hasList(player.getName())&&ConfigReader.isRegister(player.getName())){
-                    player.sendMessage(ChatColor.YELLOW+"请输入/login <密码>" );
-                }
-            }
-        }
     }
 }
